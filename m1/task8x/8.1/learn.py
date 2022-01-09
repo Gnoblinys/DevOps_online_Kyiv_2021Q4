2cd275d6b0b6a4d3cdbfbd6c5a4153de3c3046e9
Gnoblinys <it.igor.s@gmail.com>
2022-01-10 00:46:32 +0200

    8.1

--- m1/task8x/8.1/learn.py
+++ m1/task8x/8.1/learn.py
@@ -1,3 +1,4 @@
+import math
 
 def validate_param():
     count = 0
@@ -13,15 +14,50 @@ def validate_param():
         else:
             return a, b, c
 
+def discriminant(a, b, c):
+    discr = b ** 2 - 4 * a * c
+    return discr
+
+def roots(discr, a, b, c):
+    if discr > 0:
+        x1 = (-b + math.sqrt(discr)) / (2 * a)
+        x2 = (-b - math.sqrt(discr)) / (2 * a)
+        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+        results = x1, x2
+        
+    elif discr == 0:
+        x = -b / (2 * a)
+        print("x = %.2f" % x)
+    else:
+        print("Without decision")
+
+
+def solv_square(a, b, c):
+    discr = discriminant(a, b, c)
+    print("Discriminant:", str(discr))
+    roots(discr, a, b, c)
+
+def square_print(a, b, c, roots):
+    print("================\nPrint results:\nEntered parameters:")
+    print("a:", str(a), "\nb:", str(b), "\nc:", str(c))
+    print(results)
+
 
 def main():
     valid_param = validate_param()
     a = valid_param[0]
     b = valid_param[1]
     c = valid_param[2]
-    print((a), (b), (c))
+    solv_square(a, b, c)
+    square_print(a, b, c, roots)
 
 
 
 
 main()
+
+
+
+
+
+# square_print(a, b, c, roots) – выводит на экран результат 
--- m1/task8x/8.1/square.py
+++ m1/task8x/8.1/square.py
@@ -1,3 +1,4 @@
+import math
 
 def validate_param():
     count = 0
@@ -13,7 +14,52 @@ def validate_param():
         else:
             return a, b, c
 
+def discriminant(a, b, c):
+    discr = b ** 2 - 4 * a * c
+    return discr
 
-validate_param()
 
-print(a + b + c)
+
+def roots(discr, a, b, c):
+    if discr > 0:
+        x1 = (-b + math.sqrt(discr)) / (2 * a)
+        x2 = (-b - math.sqrt(discr)) / (2 * a)
+        print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+        return discr, x1, x2
+    elif discr == 0:
+        x = -b / (2 * a)
+        print("x = %.2f" % x)
+        return discr, x
+    else:
+        print("Without decision")
+
+def square_print(a, b, c, root):
+    print("================\nPrint results:\nEntered parameters:")
+    print("a:", str(a), "\nb:", str(b), "\nc:", str(c))
+    x = root[0]
+    x1 = root[1]
+    x2 = root[2]
+    print(root)
+    print(x, x1, x2)
+
+
+def solv_square(a, b, c):
+    discr = discriminant(a, b, c)
+    root = roots (discr, a, b, c)
+    square_print(a, b, c, root)
+
+
+
+def main():
+    valid_param = validate_param()
+    a = valid_param[0]
+    b = valid_param[1]
+    c = valid_param[2]
+    solv_square(a, b, c)
+
+
+
+
+main()
+
+
