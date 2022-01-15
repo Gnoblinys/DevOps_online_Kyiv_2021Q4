6b1845d5ed19ccc5cfb1f495ecc3ec49ac8a9c5e
Gnoblinys <it.igor.s@gmail.com>
2022-01-15 23:50:21 +0200

    apd

--- m1/task10x/autoinstal_apache.sh
+++ m1/task10x/autoinstal_apache.sh
@@ -4,6 +4,19 @@
 sudo apt update
 sudo apt upgrade -y
 
+sudo useradd -m web1
+sudo mkdir /home/web1/.ssh
+sudo chmod 700 /home/web1/.ssh
+sudo touch /home/web1/.ssh/authorized_keys
+sudo chmod 600 /home/web1/.ssh/authorized_keys
+
+
+
 sudo apt install apache2 -y
 sudo ufw allow in "Apache"
+
+
+
+
+
 
