0542f322cfe273f430bc141346b52a003d094fba
Gnoblinys <it.igor.s@gmail.com>
2022-01-16 22:44:36 +0200

    job is work!

--- m1/task10x/Jenkins_jobs.sh
+++ m1/task10x/Jenkins_jobs.sh
@@ -1,22 +1,26 @@
-deploy_job
-=======================================================
+Before scp
+
+1) We have to use -o StrictHostKeyChecking=no:
+scp -v -o StrictHostKeyChecking=no index.html student@192.168.88.211:/var/www/html
+instead of:
+scp -v index.html student@192.168.88.211:/var/www/html
+2) We have to copy id_rsa to /var/lib/jenkins/.ssh
+3)The /var/lib/jenkins/.ssh directory and filesinside of it should be owned by jenkins
+
+chown jenkins:jenkins id_rsa
+chmod 600 id_rsa
+
+
 echo "======Build Started======"
 cat <<EOF > index.html
 <html>
 <body bgcolor=black>
 <center>
 <h2><font color=yellow>Test Jankins Build</font></h2>
+<h2><font color=yellow>Changed 1</font></h2>
 </center>
 </body>
 </html>>
 EOF
+scp -i /var/lib/jenkins/.ssh/EC2.pem -o StrictHostKeyChecking=no index.html ubuntu@172.31.31.63:/var/www/html                    
 echo "======Build Finished====="
-
-=======================================================
-
-echo "======Test Started======"
-
-
-
-echo "======Test Finished====="
-
--- m1/task10x/autoinstal_apache.sh
+++ m1/task10x/autoinstal_apache.sh
@@ -4,11 +4,19 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo useradd -m web1
-sudo mkdir /home/web1/.ssh
-sudo chmod 700 /home/web1/.ssh
-sudo touch /home/web1/.ssh/authorized_keys
-sudo chmod 600 /home/web1/.ssh/authorized_keys
+sudo adduser web1 --disabled-password
+sudo su web1
+cd ~/
+mkdir .ssh
+chmod 700 .ssh
+touch .ssh/authorized_keys
+chmod 600 .ssh/authorized_keys
+
+ssh-keygen -m PEM -f ~/.ssh/web1 -N ""
+cd .ssh/
+mv web1 web1.pem
+
+
 
 
 
