3a7c143f72eb63aba3ebdac0100c3e8d046097dc
SinelnikIgor <it.igor.s@gmail.com>
2022-01-25 18:04:36 +0200

    disable startup wizard Jenkins

--- m1/task11x/ec2_userdata/jenkins.sh
+++ m1/task11x/ec2_userdata/jenkins.sh
@@ -15,6 +15,31 @@ sudo apt install openjdk-11-jdk -y
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
 
+
+ /etc/default/jenkins
+
+sudo sed '9a\JAVA_ARGS="-Djenkins.install.runSetupWizard=false"' /etc/default/jenkins > temp.txt
+sudo mv temp.txt /etc/default/jenkins
+
+sudo mkdir /var/lib/jenkins/init.groovy.d
+sudo cat > /var/lib/jenkins/init.groovy.d/basic-security.groovy <<TXT
+#!groovy
+
+import jenkins.model.*
+import hudson.util.*;
+import jenkins.install.*;
+
+def instance = Jenkins.getInstance()
+
+instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+
+TXT
+
+sudo service jenkins stop
+sudo service jenkins start
+
+sudo rm /var/lib/jenkins/init.groovy.d/basic-security.groovy
+
 sudo timedatectl set-timezone Europe/Kiev
 sudo apt upgrade -y
 
