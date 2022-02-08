0b3dbc252827c87b6b937a67e46e5b436a2ea0c9
Gnoblinys <it.igor.s@gmail.com>
2022-02-08 23:12:21 +0200

    Verginia is Works!

--- m1/final_project/terraform/aws_instances.tf
+++ m1/final_project/terraform/aws_instances.tf
@@ -23,32 +23,6 @@ resource "aws_eip" "elastick_ip_WEB1" {
   instance = aws_instance.WEB1.id
 }
 
-#create main vpc
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "Main"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-#create subnet in main vpc
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "Main"
-  }
-}
-
 
 # Creating instanse 1
 resource "aws_instance" "WEB1" {
@@ -57,7 +31,6 @@ resource "aws_instance" "WEB1" {
     key_name = "EC2_us-east-1a"
     user_data = file("ec2_userdata/apache.sh")
     subnet_id = aws_subnet.main.id
-    private_ip = "10.0.1.100"
     vpc_security_group_ids = [aws_security_group.allow_ALL.id]
 
     tags = {
@@ -68,35 +41,6 @@ resource "aws_instance" "WEB1" {
     }
 }
 
-
-# security_group
-resource "aws_security_group" "allow_ALL" {
-  name        = "allow_ALL"
-  vpc_id = aws_vpc.main.id
-
-  dynamic "ingress" {     #inbound trafick
-    for_each = ["22", "80", "8080"]
-    content {
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  }
-
-
-
-  egress {              #outbound trafick
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ALL"
-  }
-}
 
 
 # Output zone ====================================
