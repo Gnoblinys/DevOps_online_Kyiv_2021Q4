dc66c37f73d0a400e059a622aec825ed19bd5d3d
SinelnikIgor <it.igor.s@gmail.com>
2022-02-08 17:53:12 +0200

    add folder final project

--- m1/final_project/terraform/terraforw_aws.tf
+++ m1/final_project/terraform/terraforw_aws.tf
@@ -18,21 +18,48 @@ resource "aws_key_pair" "EC2_us-east-1a" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkt8OVZQC2DYyqf7VCj6lMJKvARg3/n/lTipxi76RhbMU8MIduI+OjyLH0oA5N10zG8KFu0ZsI6VuNvcfDlBBVcocjxezz7fjApN1+6rUvw3MGwyvgJXLIuOhaDJDg66KJjHb5tIlxBMUz3YRxpGbkcKOYjg5pRitZ9tNHOv4e07XpDOQMq4ZaACbiPxDSlgkSS4/ZSGElNetgVxI+JLutLe3Ai5ND4B1nGs178CKUudkT+TsXKcblpBGytVGDN5zucGT3nbnjjk0aAwIKPun19CQQ9EsMSHXWDklr2l0RTPPPTnIoJSNDVjN518rZXmmWugAITw8cgYaYf7HF22V7 EC2"
 }
 
-
+#elastik ip
 resource "aws_eip" "elastick_ip_WEB1" {
   instance = aws_instance.WEB1.id
 }
 
+#create main vpc
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+#create subnet in main vpc
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "Main"
+  }
+}
 
 
 # Creating instanse 1
 resource "aws_instance" "WEB1" {
     ami = "ami-04505e74c0741db8d"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.allow_ALL.id]
     key_name = "EC2_us-east-1a"
     user_data = file("ec2_userdata/apache.sh")
+    subnet_id = aws_subnet.main.id
+    private_ip = "10.0.1.100"
+    vpc_security_group_ids = [aws_security_group.allow_ALL.id]
+
     tags = {
       Name = "WEB1"
     }
@@ -45,7 +72,7 @@ resource "aws_instance" "WEB1" {
 # security_group
 resource "aws_security_group" "allow_ALL" {
   name        = "allow_ALL"
-
+  vpc_id = aws_vpc.main.id
 
   dynamic "ingress" {     #inbound trafick
     for_each = ["22", "80", "8080"]
