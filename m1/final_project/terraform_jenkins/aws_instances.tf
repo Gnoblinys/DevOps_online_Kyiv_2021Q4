4841d13bce27383600e27a2fa2ff234be9d6b808
SinelnikIgor <it.igor.s@gmail.com>
2022-02-10 18:27:08 +0200

    cnanged .tf

--- m1/final_project/terraform_jenkins/aws_instances.tf
+++ m1/final_project/terraform_jenkins/aws_instances.tf
@@ -1,5 +1,12 @@
+# Connect to AWS
+#Use next comand for add KEYs to system.
+#Linux
+#export AWS_ACCESS_KEY_ID=
+#export AWS_SECRET_ACCESS_KEY=
 
-
+#Windows
+#set AWS_ACCESS_KEY_ID=
+#set AWS_SECRET_ACCESS_KEY=
 
 terraform {
   required_providers {
@@ -9,8 +16,6 @@ terraform {
     }
   }
 }
-
-
 provider "aws" {
     region = "us-east-1"
 }
@@ -21,30 +26,22 @@ resource "aws_key_pair" "EC2_us-east-1a" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkt8OVZQC2DYyqf7VCj6lMJKvARg3/n/lTipxi76RhbMU8MIduI+OjyLH0oA5N10zG8KFu0ZsI6VuNvcfDlBBVcocjxezz7fjApN1+6rUvw3MGwyvgJXLIuOhaDJDg66KJjHb5tIlxBMUz3YRxpGbkcKOYjg5pRitZ9tNHOv4e07XpDOQMq4ZaACbiPxDSlgkSS4/ZSGElNetgVxI+JLutLe3Ai5ND4B1nGs178CKUudkT+TsXKcblpBGytVGDN5zucGT3nbnjjk0aAwIKPun19CQQ9EsMSHXWDklr2l0RTPPPTnIoJSNDVjN518rZXmmWugAITw8cgYaYf7HF22V7 EC2"
 }
 
+
+
 #elastik ip
 resource "aws_eip" "elastick_ip_WEB_2" {
   instance = aws_instance.WEB_2.id
 }
 
-
-
-resource "aws_network_interface" "eth0" {
-  subnet_id = aws_subnet.main.id
-  private_ips     = ["10.0.1.100"]
-  security_groups = [aws_security_group.allow_ALL.id]
-}
-
-
 # Creating instanse
 resource "aws_instance" "WEB_2" {
-    ami = "ami-04505e74c0741db8d"
+    ami = "ami-01f87c43e618bf8f0"
     instance_type = "t2.micro"
     key_name = "EC2_us-east-1a"
     user_data = file("apache.sh")
-    network_interface {
-    network_interface_id = aws_network_interface.eth0.id
-    device_index         = 0
-  }
+    subnet_id   = aws_subnet.main.id
+    security_groups = [aws_security_group.allow_ALL.id]
+
     tags = {
       Name = "WEB_2"
     }
@@ -58,7 +55,7 @@ resource "aws_instance" "WEB_2" {
 # Output zone ====================================
 
 output "WEB_2_publick_ip" {
-  value = aws_eip.elastick_ip_WEB_2.public_ip
+  value = aws_instance.WEB_2.public_ip
 }
 output "WEB_2_private_ip" {
   value = aws_instance.WEB_2.private_ip
